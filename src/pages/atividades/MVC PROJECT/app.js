86e763505e11bc5b8de288b3a4088fc0a8fadcbc
RenanAlves <ra9835522@gmail.com>
2023-10-30 19:07:58 -0300

    MVC deletado de atividades para correção de conflitos com new-features

--- src/pages/atividades/MVC PROJECT/app.js
+++
@@ -1,5 +0,0 @@
-import {controller } from "./src/controller.js";
-document.addEventListener('DOMContentLoaded', ()=>{
-    controller.iniciar();
-})
-
--- src/pages/atividades/MVC PROJECT/src/controller.js
+++
@@ -1,9 +0,0 @@
-import { model } from "./model/model.js";
-import { construtorGrafico } from "./view.js";
-
-const controller = {
-    iniciar:() =>{
-        construtorGrafico.render(model)
-    }
-}
-export{controller}
